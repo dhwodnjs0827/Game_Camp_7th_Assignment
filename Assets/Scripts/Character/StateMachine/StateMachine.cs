59a165386b83268b90d4433fb508734f896ea2b6
Jaewon <dhwodnjs0827@naver.com>
2025-08-17 17:55:12 +0900

    [Fix] 상태 전이 오류 수정

--- Assets/Scripts/Character/StateMachine/StateMachine.cs
+++ Assets/Scripts/Character/StateMachine/StateMachine.cs
@@ -2,14 +2,17 @@ public class StateMachine
 {
     private IState previousState;
     private IState currentState;
-    
+
     public IState PreviousState => previousState;
     public IState CurrentState => currentState;
 
     public void ChangeState(IState newState)
     {
-        previousState = currentState;
-        previousState?.Exit();
+        currentState?.Exit();
+        if (currentState != newState)
+        {
+            previousState = currentState;
+        }
         currentState = newState;
         currentState.Enter();
     }
